2549b8bde1a2ea1e720bbfb49a679adf38c8feff
zouyunhang <2634189224@qq.com>
2026-08-09 00:25:23 +0800

    关于页删除顶部图片，首页文案调整

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yunhang';
+export const SITE_DESCRIPTION = '昀航的个人博客 —— 记录 Java 后端与 AI Agent 的技术实践与思考';
